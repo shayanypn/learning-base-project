1e670b3839de6d7b5d6e4c2be7784673dc94839c
shayanypn <shayanypn@gmail.com>
2021-03-20 08:29:24 +0330

    add a boilerplate

--- nextjs-testing/pages/index.tsx
+++ nextjs-testing/pages/index.tsx
@@ -1,52 +1,70 @@
-import * as React from 'react'
-import { InferGetStaticPropsType } from 'next'
-import AddPost from '../components/AddPost'
-import Post from '../components/Post'
-import { IPost } from '../types'
+import Head from 'next/head';
+import React from 'react';
+import styles from '../styles/Home.module.css';
 
-const BASE_URL: string = 'https://jsonplaceholder.typicode.com/posts'
+const Home: React.FC = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-export default function IndexPage({
-  posts,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
-  const [postList, setPostList] = React.useState(posts)
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        Welcome to
+        {' '}
+        <a href="https://nextjs.org">Next.js!</a>
+      </h1>
 
-  const addPost = async (e: React.FormEvent, formData: IPost) => {
-    e.preventDefault()
-    const post: IPost = {
-      id: Math.random(),
-      title: formData.title,
-      body: formData.body,
-    }
-    setPostList([post, ...postList])
-  }
+      <p className={styles.description}>
+        Get started by editing
+        {' '}
+        <code className={styles.code}>pages/index.js</code>
+      </p>
 
-  const deletePost = async (id: number) => {
-    const posts: IPost[] = postList.filter((post: IPost) => post.id !== id)
-    console.log(posts)
-    setPostList(posts)
-  }
+      <div className={styles.grid}>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+          <h3>Documentation &rarr;</h3>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
 
-  if (!postList) return <h1>Loading...</h1>
+        <a href="https://nextjs.org/learn" className={styles.card}>
+          <h3>Learn &rarr;</h3>
+          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        </a>
 
-  return (
-    <main className='container'>
-      <h1>My posts</h1>
-      <AddPost savePost={addPost} />
-      {postList.map((post: IPost) => (
-        <Post key={post.id} deletePost={deletePost} post={post} />
-      ))}
+        <a
+          href="https://github.com/vercel/next.js/tree/master/examples"
+          className={styles.card}
+        >
+          <h3>Examples &rarr;</h3>
+          <p>Discover and deploy boilerplate example Next.js projects.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+        >
+          <h3>Deploy &rarr;</h3>
+          <p>
+            Instantly deploy your Next.js site to a public URL with Vercel.
+          </p>
+        </a>
+      </div>
     </main>
-  )
-}
 
-export async function getStaticProps() {
-  const res = await fetch(BASE_URL)
-  const posts: IPost[] = await res.json()
+    <footer className={styles.footer}>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by
+        {' '}
+        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      </a>
+    </footer>
+  </div>
+);
 
-  return {
-    props: {
-      posts,
-    },
-  }
-}
+export default Home;
